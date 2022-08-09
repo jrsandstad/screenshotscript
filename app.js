dd8a724fdf20e61823b101aad9268bd29ab3a304
Jonas Sandstad <jonas.sandstad@outlook.com>
2022-08-09 15:00:41 +0200

    Added folder array and comments

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const fs                = require('fs');
 const screenshot        = require('screenshot-desktop');
 const includeFile       = "/Users/jonas/Desktop/screenshotscript/Include.txt"; //Erstatt med Windows filepath
 const locations         = "/Users/jonas/Desktop/screenshotscript/Lokasjoner";  //Erstatt med Windows filepath
-const wait              = 5; //Waiting time before script runs again
+const wait              = 5; //Update this to set how many minutes the script will wait before running the function again. Currently 5 minutes.
+const minute            = 6000; //6 seconds in ms. Update to 1 minute = 60000 ms in final version
+var folders             = []; //Stores names of folders to place the screenshot in after locations.txt have been read.
 
 
-function takeScreenshot(){
+function takeScreenshot(){ //Takes a screenshot and saves it in the project directory as "Test.png"
     screenshot({filename: 'Test.png'});
     console.log("Screenshot taken.");
 }
 
-setInterval(takeScreenshot, 30000);
+setInterval(takeScreenshot, wait*minute); //Runs the takeScreenshot function every x ms where x is the result of multiplication of the wait and minute constants.
