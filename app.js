acf8d9b7b35dc22eb326993f44afa5a724469c76
Jonas Sandstad <jonas.sandstad@outlook.com>
2022-08-10 07:16:38 +0200

    Added file reader

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const fs                = require('fs');
 const screenshot        = require('screenshot-desktop');
-const includeFile       = "/Users/jonas/Desktop/screenshotscript/Include.txt"; //Erstatt med Windows filepath
-const locations         = "/Users/jonas/Desktop/screenshotscript/Lokasjoner";  //Erstatt med Windows filepath
+const locations         = "locations.txt";  //Erstatt med Windows filepath
 const wait              = 5; //Update this to set how many minutes the script will wait before running the function again. Currently 5 minutes.
 const minute            = 6000; //6 seconds in ms. Update to 1 minute = 60000 ms in final version
 var folders             = []; //Stores names of folders to place the screenshot in after locations.txt have been read.
@@ -11,5 +10,11 @@ function takeScreenshot(){ //Takes a screenshot and saves it in the project dire
     screenshot({filename: 'Test.png'});
     console.log("Screenshot taken.");
 }
+const array = fs.readFileSync(locations).toString().split("\n");
+for (const a of array) {
+  console.log(a);
+}
 
-setInterval(takeScreenshot, wait*minute); //Runs the takeScreenshot function every x ms where x is the result of multiplication of the wait and minute constants.
+console.log(array);
+
+//setInterval(takeScreenshot, wait*minute); //Runs the takeScreenshot function every x ms where x is the result of multiplication of the wait and minute constants.
